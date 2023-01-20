87c9273840278cf78c2f5862871f9977e6edc659
Jerry John Thomas <jerryjohnthomastvm@gmail.com>
2023-01-20 18:15:14 +0530

    added multiple routes in warehouse

--- client/src/warehouse/Directions2.js
+++ client/src/warehouse/Directions2.js
@@ -23,21 +23,80 @@ import {
 import { useRef, useState } from 'react'
 import { useEffect } from 'react'
 import { addScaleCorrector } from 'framer-motion'
+import RiderCard from './RiderCard'
 
 // const center = { lat: 48.8584, lng: 2.2945 }
-const center = { lat: 10, lng: 78 }
+const center = { lat: 9.95, lng: 76.25 }
 
 function Directions2() {
 
-  // useEffect(()=>{
-  //   console.log("use effect");
-  //   let api_key = process.env.REACT_APP_GOOGLE_MAPS_API;
-  //   console.log(api_key);
-  // })
+
+    const [rider_places, setRiderplaces] = useState([])
+    const [renderitem, setRenderitem] = useState([])
+
+    // const colors=["yellow", "red" , "blue"]
+    const colors = [  "#A0E6FF",  "#FF8A80",  "#A4D3EE",  "#FFA07A",  "#90CAF9",  "#FF6347",  "#81D4FA",  "#FF7F50",  "#7FC4FD",  "#FF4500"]
+  useEffect(()=>{
+
+    setRiderplaces([
+        [{rider_id: 1},{lat : 10.7992017 , lng:76.8221794},{lat:13.7992017 , lng:77.8221794}, {lat:12.5992017 , lng:76.9221794}, {lat:12.7992017 , lng:77.8221794}],
+        [{rider_id: 2},{lat : 10.9992017 , lng:76.9221794}, {lat:12.6992017 , lng:77.5221794}],
+    ])
+},[])
+
+    useEffect(()=>
+    {
+        if(window.google)
+            console.log("window.google is up")
+        else
+        {
+            console.log("window.google is not up")
+            return;
+        }
+
+
+        console.log(rider_places)
+
+        for (let index in rider_places )
+        {
+            let data = rider_places[index]
+            let data_transformed = []
+            
+            let rider_id = data[0]["rider_id"];
+            data.shift();
+            console.log("rider " + rider_id+ " with " + data.length +" points ");
+            
+            
+            // eslint-disable-next-line no-undef
+            for(let i=0;i<data.length;i++)
+            {
+                let temp = new window.google.maps.LatLng(data[i].lat, data[i].lng);
+                data_transformed.push(temp);
+            }
+            
+            let origin = data_transformed[0];
+            let waypoints_temp = data_transformed.slice(1, data_transformed.length - 1)
+            let waypoints = []
+            for (let ind3 in waypoints_temp)
+                waypoints.push({location:waypoints_temp[ind3]})
+            let destination = data_transformed[data_transformed.length-1];
+
+            // console.log("origin")
+            // console.log(origin)
+            // console.log("waypoints")
+            // console.log(waypoints)
+            // console.log("destination")
+            // console.log(destination)
+
+            let res = calculateRouteTuples(origin, waypoints, destination)
+            console.log("returned calculated routes")
+
+        }
+
+    },[rider_places,window.google])
 
   
   const { isLoaded } = useJsApiLoader({
-    // googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API,
     googleMapsApiKey: 'AIzaSyC-BWemSByl9AoF7KNOzaFDL503NNrjB_g',
     libraries: ['places'],
   })
@@ -73,6 +132,26 @@ function Directions2() {
     });
   }
 }
+  async function calculateRouteTuples(org, wp, dest) {
+   
+    const directionsService = new window.google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: org,
+      waypoints : wp,
+      destination: dest,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+
+    setRenderitem( renderitem => [...renderitem, results])
+    setDirectionsResponse(results)
+
+    // setDistance(results.routes[0].legs[0].distance.text)
+    // setDuration(results.routes[0].legs[0].duration.text)
+    console.log("over");
+    return "okk";
+  }
+
 
   async function calculateRoute() {
     if (originRef.current.value === '' || destiantionRef.current.value === '') {
@@ -137,16 +216,27 @@ function Directions2() {
             onLoad={map => setMap(map)}
             >
           <Marker position={center} />
-          {directionsResponse && (
-              <DirectionsRenderer directions={directionsResponse} options={{polylineOptions:{strokeColor:"yellow"}}} />
-              )}
-          
+          {
+              renderitem.map((data,index) => {
+                return(
+                    <div>
+                        <DirectionsRenderer directions={data} options={{polylineOptions:{strokeColor:colors[index]}}} />
+                    </div>
+                )
+                }
+            )
+          }
+
         </GoogleMap>
         </div>
 
 
         <div className='jerry_directions2_rider_info'>
-            Rider Component
+          {
+            renderitem.map((data,index)=>(
+                <RiderCard />
+            ))
+          }
 
         </div>
 
---
+++ client/src/warehouse/RiderCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./RiderCard.css"
+
+function RiderCard() {
+  return (
+    <div className='jerry_ridercard_container'>
+        <div>Name</div>
+        <div>Distance</div>
+        <div>Time</div>
+    </div>
+  )
+}
+
+export default RiderCard
