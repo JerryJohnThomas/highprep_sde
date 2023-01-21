20da17b9a73cadd702ebb66a18f18c698f8c3b8a
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-01-21 07:16:37 +0530

    Implementing the start algo. HAlf completed

---
+++ backend/algo_apis/maps_route_api.py
@@ -0,0 +1,72 @@
+import numpy as np
+import json
+import requests
+
+def api_call_dummy(a,b):
+    size1 = len(a)
+    size2 = len(b)
+    ans1 = np.random.rand(size1,size2)
+    ans2 = np.random.rand(size1,size2)
+    return ans1, ans2
+
+def api_call(origins, destinations):
+    # origins is an array where each item is an aarray [index, latitiude ,longitide]
+    size1 = len(origins)
+    size2 = len(destinations)
+    
+    distance_res = np.zeros([size1,size2])
+    time_res = np.zeros([size1,size2])
+
+    body = { "origins": [], "destinations" : [], "travelMode": "DRIVE",
+    #   "routingPreference": "TRAFFIC_AWARE"        ## uncommenting this will make the api higher priced and can do a max of 10 points as opposed to 25 points in a singel call
+    }
+
+    for location in origins:
+        body['origins'].append({"waypoint": {"location": {"latLng": {"latitude": location[1], "longitude": location[2]}}}})
+    
+    for location in destinations:
+        body['destinations'].append({"waypoint": {"location": {"latLng": {"latitude": location[1], "longitude": location[2]}}}})
+
+    # print(body)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': 'AIzaSyC-BWemSByl9AoF7KNOzaFDL503NNrjB_g',
+        'X-Goog-FieldMask': 'originIndex,destinationIndex,duration,distanceMeters,status,condition',
+    }
+
+    call = requests.post('https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix', json=body, headers=headers)
+    res = call.text
+
+    print(call.status_code, call.reason)
+    json_object = json.loads(call.text)
+
+    print(len(json_object))
+    for id,data in enumerate(json_object):
+        # print(id, data)
+        ind1 = data["originIndex"]
+        ind2 = data["destinationIndex"]
+        time = int(data["duration"][:-1])
+        
+        if ind1 == ind2 or time==0:
+            dist = 0
+        else:
+            dist = data["distanceMeters"]
+
+        distance_res[ind1][ind2] = dist
+        time_res[ind1][ind2] = time
+
+    return distance_res , time_res
+
+origins = [[0, 12.91345, 77.57498], [1, 12.94022, 77.53995], [2, 12.91275, 77.58582], [3, 12.94022, 77.53995], [4, 12.90662, 77.59663], [5, 12.86796, 77.58457], [6, 12.90826, 77.61137], [7, 12.90206, 77.58585], [8, 12.89096, 77.5834], [9, 12.98276, 77.60712], [10, 12.90012, 77.59566], [11, 12.89986, 77.58587], [12, 12.91054, 77.59913], [13, 12.90641, 77.59292], [14, 12.90756, 77.59837], [15, 12.90445, 77.59639], [16, 12.90611, 77.59661], [17, 12.88876, 77.58247], [18, 12.8964, 77.58617], [19, 12.89986, 77.58587], [20, 12.87544, 77.58334], [21, 12.88557, 77.58231], [22, 12.90654, 77.59849], [23, 12.89838, 77.59681], [24, 12.90258, 77.59664]]
+dest = [[50, 12.89963, 77.64903], [51, 12.9319, 77.60736], [52, 12.91586, 77.64479], [53, 12.91966, 77.65186], [54, 12.91199, 77.6518], [55, 12.9121, 77.64958], [56, 12.89757, 77.65878], [57, 12.9366, 77.6148], [58, 12.91248, 77.65201], [59, 12.91232, 77.65167], [60, 12.91576, 77.64931], [61, 12.91586, 77.64479], [62, 12.91248, 77.65201], [63, 12.91951, 77.64992], [64, 12.91575, 77.64901], [65, 12.91967, 77.65105], [66, 12.91595, 77.64929], [67, 12.91279, 77.6536], [68, 12.91967, 77.65105], [69, 12.91182, 77.64814], [70, 12.91529, 77.64891], [71, 12.92212, 77.65145], [72, 12.91571, 77.65253], [73, 12.91571, 77.65253], [74, 12.9215, 77.65155]]
+
+origins3 = [[0, 12.91345, 77.57498], [1, 12.94022, 77.53995], [2, 12.91275, 77.58582]]
+dest3 = [[50, 12.89963, 77.64903], [51, 12.9319, 77.60736], [52, 12.91586, 77.64479]]
+
+# api_call(origins, dest)
+# api_call(origins3, dest3)
+
+# def api_call(a,b):
+#     return api_call_dummy(a,b)
+    
--- backend/algo_apis/views.py
+++ backend/algo_apis/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import pandas as pd;
+import time 
+import requests
 import csv
+import numpy as np;
+import json
+# from maps_route_api import api_call
+from datetime import datetime as dt
+import googlemaps
+import gmaps
+
 
 # endpoint to return the lat-long of the places from the google maps 
 class LatLongView(APIView):
@@ -33,21 +42,211 @@ class UploadExcelSheetView(APIView):
 
 
 
+
+def api_call_dummy(a,b):
+    size1 = len(a)
+    size2 = len(b)
+    ans1 = np.random.rand(size1,size2)
+    ans2 = np.random.rand(size1,size2)
+    return ans1, ans2
+
+def api_call(origins, destinations):
+    # origins is an array where each item is an aarray [index, latitiude ,longitide]
+    size1 = len(origins)
+    size2 = len(destinations)
+    
+    distance_res = np.zeros([size1,size2])
+    time_res = np.zeros([size1,size2])
+
+    body = { "origins": [], "destinations" : [], "travelMode": "DRIVE",
+    #   "routingPreference": "TRAFFIC_AWARE"        ## uncommenting this will make the api higher priced and can do a max of 10 points as opposed to 25 points in a singel call
+    }
+
+    for location in origins:
+        body['origins'].append({"waypoint": {"location": {"latLng": {"latitude": location[1], "longitude": location[2]}}}})
+    
+    for location in destinations:
+        body['destinations'].append({"waypoint": {"location": {"latLng": {"latitude": location[1], "longitude": location[2]}}}})
+
+    print("origin 23", origins[23])
+    print("origin 23", origins[24])
+    print("destination 13", origins[13])
+
+    # print(body)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': 'AIzaSyC-BWemSByl9AoF7KNOzaFDL503NNrjB_g',
+        'X-Goog-FieldMask': 'originIndex,destinationIndex,duration,distanceMeters,status,condition',
+    }
+
+    call = requests.post('https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix', json=body, headers=headers)
+    res = call.text
+
+    print(call.status_code, call.reason)
+    json_object = json.loads(call.text)
+
+    print(len(json_object))
+    for id,data in enumerate(json_object):
+        # print(id, data)
+        print(data)
+        ind1 = data["originIndex"]
+        ind2 = data["destinationIndex"]
+
+        time = int(data["duration"][:-1])
+
+
+        if ind1 == ind2 or time==0:
+            dist = 0
+        else:
+            dist = data["distanceMeters"]
+
+        distance_res[ind1][ind2] = dist
+        time_res[ind1][ind2] = time
+
+    return distance_res , time_res
+
+
+
+gmaps = googlemaps.Client(key='AIzaSyC-BWemSByl9AoF7KNOzaFDL503NNrjB_g')
+
+def get_geocordinates_not_working(place):
+
+    place= place.replace(" ", "+ ")
+    url = "https://discover.search.hereapi.com/v1/discover?at=12.7307999,77.9973085&limit=2&q="+place+"&apiKey=6QNNTzC1zysibIBzUnTnBAg5KY1Qc4BBEBElSnrMvSo"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:    # Check if request was successful
+        data = response.json()         # Get JSON result
+        title = data['items'][0]['title']
+        geo = data['items'][0]['position']
+        lat = geo['lat']
+        lng = geo['lng']
+        print("title: ", title)
+        print("lat: ", lat)
+        print("lng: ", lng)
+
+        return lat, lng, title
+    return 1000, 1000, "NIL" 
+
+
+def get_geocordinates(data):
+
+    place = data.replace(" ", "+ ")
+    geocode_result= gmaps.geocode(place)
+    lat = geocode_result[0]['geometry']['location'] ['lat']
+    lng = geocode_result[0]['geometry']['location'] ['lng']
+    print("lat: ", lat)
+    print("lng: ", lng)
+    return lat, lng, place
+    
+def rupesh_test1():
+
+    place="palakkad"    
+    geocode_result= gmaps.geocode(place)
+    lat = geocode_result[0]['geometry']['location'] ['lat']
+    lng = geocode_result[0]['geometry']['location'] ['lng']
+    print(lat);
+    print(lng);
+    return lat, lng, place
+    
+
+
+# defining the function to convert the address to lat long 
+def addressToLocations(excelPath):
+    
+    data = pd.read_excel(excelPath)
+    places = data['address']
+    limit = 218
+    data["lat"] =-1.00
+    data["lng"] =-1.00
+
+    for i in range(places.size):
+        x =places[i]
+        lat, lng, title = get_geocordinates(x)
+        print(title)
+        if lat==1000:
+            continue
+        print()
+        data['lat'][i]= lat
+        data['lng'][i]= lng
+        time.sleep(1)
+        print(i," over")
+
+    data.to_csv("./data/bangalore_dispatch_address_finals_out.csv")
+
+
+
+# function to get the distance time matrix 
+def calculateDistanceTimeMatrix(filePath):
+    
+    df = pd.read_csv(filePath)
+    lat = df["lat"]
+    lng = df["lng"]
+
+    size=lat.size
+
+    print()
+
+    num_index = np.arange(0,size)
+
+    places = []
+    for i in range(size):
+        places.append([i,lat[i],lng[i]])
+
+    batches = []
+    count=0
+    for i in range(0,size,25):
+        batches.append(places[i:min(i+25,size)])
+
+
+    arr = np.arange(0,size) 
+    dist_mat = pd.DataFrame( index=arr, columns=arr)
+    time_mat = pd.DataFrame( index=arr, columns=arr)
+
+    print("total batches ", len(batches))
+
+    for id_i,data_i in enumerate(batches):
+        id_range_i = np.arange(id_i*25, id_i*25 + len(data_i))
+        
+        for id_j,data_j in enumerate(batches):
+            id_range_j = np.arange(id_j*25, id_j*25 + len(data_j))
+            
+            # print(id_range_i," XX ",id_range_j)
+            dist_batch, time_batch = api_call(data_i,data_j)
+            # updaing the csv
+            for j in id_range_j:
+                for i in id_range_i:
+                    # print("accessing indices", i-id_range_i[0])
+                    key_i = i-id_range_i[0]
+                    key_j = j-id_range_j[0]
+                    dist_mat.at[int(i),str(j)] = dist_batch[key_i][key_j]
+                    time_mat.at[int(i),str(j)] = time_batch[key_i][key_j]
+            time.sleep(15)
+            print(id_i," and ",id_j," is over")
+
+    time_now = dt.now().isoformat()
+    time_now= time_now.replace(":",".")
+
+    write_csv=True
+
+    if write_csv:
+        dist_mat.to_csv("distance_matrix"+str(size)+"_"+time_now+".csv", index=False)
+        time_mat.to_csv("time_matrix"+str(size)+"_"+time_now+".csv", index=False)
+
+
+
 # endpoint to start the algorithm once warehouse guy presses start algo 
 class StartAlgoView(APIView):
     # post request to start the algo 
     def post(self, request):
-        file = open('./data/bangalore_dispatch_address_finals_out.csv')
-        csvreader = csv.reader(file);
+        # excelPath = "./data/bangalore_dispatch_address_finals.xlsx"
+        # addressToLocations(excelPath)
+        filePath = "./data/bangalore_dispatch_address_finals_out.csv";
+        calculateDistanceTimeMatrix(filePath)
 
-        print(csvreader);
-
-        rows = [];
-        d = dict();
-
-        for row in csvreader:
-            rows.append(row);
-            print("The row is \n\n", row);
+        # rupesh_test1();
 ########################################################################################################
         # TODO
             # 1.fetch the excel sheet consisting of the list of places and the available rider 
@@ -61,14 +260,6 @@ class StartAlgoView(APIView):
         # main problems will be to 
 
 ###########################################################################################################
-
-
-        # implemeting for testing purpose by hardcoding
-        # ridersVsLocations = {};
-        # ridersVsLocations['1'] = 
-        # lat = [12.91345, 12.94022, ]
-
-
-        return Response("this endpoint is to start the algorithm to return the list of riders and their ordered set of locations");
+        return Response("hopefully should be done ");
 
 
